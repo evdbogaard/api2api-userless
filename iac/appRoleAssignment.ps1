5a361ec42d218e4b3e42251a05d4b1923299abe8
Erwin van den Bogaard <evdbogaard@gmail.com>
2023-10-12 15:47:28 +0200

    Make script handle multiple appRoles

--- iac/appRoleAssignment.ps1
+++ iac/appRoleAssignment.ps1
@@ -1,7 +1,43 @@
+function Add-AppRoleAssignment {
+    param(
+        [string]$appRole,
+        [Object]$existingAppRoles,
+        [string]$graphUrl,
+        [Object]$headers,
+        [string]$principalId,
+        [string]$appRegistrationId
+    )
+
+    $appRoleId = ($existingAppRoles | Where-Object {$_.Value -eq $appRole }).Id
+
+    if (!$appRoleId) {
+        throw "App role ${appRole} not found in app registration ${appRegistrationId}"
+    }
+
+    # App role assignment
+    $appAssignments = Invoke-RestMethod -Method 'Get' -Uri "${graphUrl}/${principalId}/appRoleAssignments" -Headers $headers
+
+    $exists = $appAssignments.value | Where-Object {$_.appRoleId -eq $appRoleId}
+
+    if ($exists) {
+        Write-Host "App role assignment already existed for ${appRole} : ${principalId}"
+    } else {
+        Write-Host "Creating new app role assignment for ${appRole} : ${principalId}"
+
+        $body = @{
+            principalId = $principalId
+            resourceId = $appRegistrationId
+            appRoleId = $appRoleId
+        } | ConvertTo-Json
+
+        Invoke-RestMethod -Method "Post" -ContentType "application/json" -Uri "${graphUrl}/${principalId}/appRoleAssignments" -Headers $headers -Body $body
+    }
+}
+
 $tenantId = $env:tenantId
 $apiPrincipalId = $env:principalId
 $appRegistrationId = $env:appRegistrationId
-$roleToSet = "server-api"
+$roles = $env:roles
 
 # Graph setup
 $graphToken = $(Get-AzAccessToken -Tenant $tenantId -ResourceUrl "https://graph.microsoft.com").Token
@@ -11,26 +47,8 @@ $headers = @{Authorization = "Bearer $graphToken"}
 # App registration / getting custom role ids
 $appRegistration = Invoke-RestMethod -Method "Get" -Uri "${graphUrl}/${appRegistrationId}" -Headers $headers
 $appRoles = $appRegistration.appRoles
-$appRoleId = ($appRoles | Where-Object {$_.Value -eq $roleToSet }).Id
 
-# App role assignment
-$appAssignments = Invoke-RestMethod -Method 'Get' -Uri "${graphUrl}/${apiPrincipalId}/appRoleAssignments" -Headers $headers
-
-$exists = $appAssignments.value | Where-Object {$_.appRoleId -eq $appRoleId}
-
-if ($exists) {
-    Write-Host "App role assignment already existed for ${apiPrincipalId}"
-} else {
-    Write-Host "Creating new app role assignment for ${apiPrincipalId}"
-
-    $body = @{
-        principalId = $apiPrincipalId
-        resourceId = $appRegistrationId
-        appRoleId = $appRoleId
-    } | ConvertTo-Json
-
-
-    Invoke-RestMethod -Method "Post" -ContentType "application/json" -Uri "${graphUrl}/${apiPrincipalId}/appRoleAssignments" -Headers $headers -Body $body
+$rolesArray = $roles.split(",") | ForEach-Object { 
+    Add-AppRoleAssignment -appRole $_ -existingAppRoles $appRoles -graphUrl $graphUrl -headers $headers -principalId $apiPrincipalId -appRegistrationId $appRegistrationId
 }
 
-# Set-Item -Path env:principalId -Value 1234
